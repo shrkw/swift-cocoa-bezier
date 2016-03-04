5218831b76ceea069e9b81271c456cd03316c141
Hiroyuki Shirakawa <hiroyuki.shirakawa@viibar.com>
2016-03-04 10:25:03 +0900

    Initial Commit

---
+++ BezierSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BezierSample
+//
+//  Created by shirakawa hiroyuki on 2016/03/04.
+//  Copyright © 2016年 shirakawa hiroyuki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BezierSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BezierSample
+//
+//  Created by shirakawa hiroyuki on 2016/03/04.
+//  Copyright © 2016年 shirakawa hiroyuki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
